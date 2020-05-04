0533a761b11c59ee08b8bd1ae2c9c332e195e70b
Riley <vishnukey@gmail.com>
2020-05-04 02:00:42 -0600

    Basic player movement and a first attempt at lighting

---
+++ Assets/Shaders/Test.shader
@@ -0,0 +1,33 @@
+﻿ Shader "Test/SharpLights" {
+        Properties {
+            _MainTex ("Texture", 2D) = "white" {}
+            _Color ("Color", Color) = (1,1,1,1)
+        }
+        SubShader {
+        Tags { "RenderType" = "Opaque" }
+        CGPROGRAM
+          #pragma surface surf SimpleLambert
+
+          fixed4 _Color;
+  
+          half4 LightingSimpleLambert (SurfaceOutput s, half3 lightDir, half atten) {
+              half NdotL = dot(s.Normal, lightDir);
+              half4 c;
+              c.rgb = s.Albedo * _LightColor0.rgb * step(0.5, NdotL * atten) * (NdotL * atten);
+              c.a = s.Alpha;
+              return c;
+          }
+  
+        struct Input {
+            float2 uv_MainTex;
+        };
+        
+        sampler2D _MainTex;
+        
+        void surf (Input IN, inout SurfaceOutput o) {
+            o.Albedo = tex2D (_MainTex, IN.uv_MainTex).rgb * _Color;
+        }
+        ENDCG
+        }
+        Fallback "Diffuse"
+    }
---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class Player : MonoBehaviour
+{
+    public float speed;
+    public Camera mainCamera;
+
+    CharacterController controller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var direction = new Vector3(
+            Input.GetAxis("Horizontal"),
+            0,
+            Input.GetAxis("Vertical")
+        ).normalized;
+
+        controller.Move(direction * speed * Time.deltaTime);
+        
+        //Debug.Log(Input.mousePosition);
+        //transform.LookAt(new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y));
+        Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hitinfo;
+        if (Physics.Raycast(ray, out hitinfo))
+        {
+            var lookPoint = new Vector3(hitinfo.point.x, transform.position.y, hitinfo.point.z);
+            
+            transform.LookAt(lookPoint);
+        }
+        
+    }
+}
