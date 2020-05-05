bfae1cc50b515302cd5ab9e0db49cbbb5633c094
Riley <vishnukey@gmail.com>
2020-05-04 18:13:34 -0600

    Started adding some systems. Trees get destroyed and increase wood number, campfire reduces wood number and increases torch number

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -5,16 +5,46 @@ using UnityEngine;
 [RequireComponent(typeof(CharacterController))]
 public class Player : MonoBehaviour
 {
+#region public_readonly
+    public int wood {
+        get;
+        private set;
+    }
+    public int arrows {
+        get;
+        private set;
+    }
+    public int torches {
+        get;
+        private set;
+    }
+
+    public int health{
+        get;
+        private set;
+    }
+#endregion
+
+#region public_props
     public float speed;
     public float turnSpeed;
+    public float reach;
     public Camera mainCamera;
+    public int max_health;
+    public LayerMask groundLayer;
+    public LayerMask interactionLayer;
+#endregion
 
+#region internal
     CharacterController controller;
+#endregion
 
+#region Unity_builtins
     // Start is called before the first frame update
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        health = max_health;
     }
 
     // Update is called once per frame
@@ -31,14 +61,69 @@ public class Player : MonoBehaviour
         //Debug.Log(Input.mousePosition);
         //transform.LookAt(new Vector3(Input.mousePosition.x, 0, Input.mousePosition.y));
         Ray ray = mainCamera.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hitinfo;
-        if (Physics.Raycast(ray, out hitinfo))
+        RaycastHit groundHit;
+        if (Physics.Raycast(ray, out groundHit, Mathf.Infinity, groundLayer))
         {
-            var lookPoint = new Vector3(hitinfo.point.x, transform.position.y, hitinfo.point.z);
+            var lookPoint = new Vector3(groundHit.point.x, transform.position.y, groundHit.point.z);
             
             //transform.LookAt(lookPoint);
             transform.SmoothLookAt(lookPoint, turnSpeed);
         }
-        
+
+        if (Input.GetMouseButtonDown(0)){
+            RaycastHit interactionHit;
+            if (Physics.Raycast(ray, out interactionHit, Mathf.Infinity, interactionLayer))
+            {
+                var interaction = interactionHit.collider.tag;
+                var position = interactionHit.collider.transform.position;
+                var go = interactionHit.collider.gameObject;
+                var sqrDist = Vector3.SqrMagnitude(transform.position - position);
+                Debug.Log($"Interaction hit! Go: {go.name}, Tag: {interaction}, Dist: {sqrDist}, Reach: {reach}, SqrReach: {reach * reach}, CloseEnough? {sqrDist <= reach * reach}");
+
+
+                if (sqrDist > reach * reach) goto escape;
+
+                switch(interaction)
+                {
+                    case "Tree":
+                        InteractTree(go);
+                        break;
+                    case "Villager":
+                        InteractVillager(go);
+                        break;
+                    case "Campfire":
+                        InteractCampfire(go);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
+        escape:
+            return;
     }
+
+    void InteractCampfire(GameObject fire)
+    {
+        if (wood >= 15){
+            wood -= 15;
+            torches++;
+        }
+    }
+
+    void InteractTree(GameObject tree)
+    {
+        Destroy(tree);
+        wood += 10;
+    }
+
+    void InteractVillager(GameObject villager)
+    {
+        if (wood >= 1){
+            wood--;
+            arrows++;
+        }
+    }
+#endregion
 }
---
+++ Assets/scripts/UIManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UIManager : Singleton<UIManager>
+{
+
+    const string WOOD_LEADER = "Wood: ";
+    const string ARROW_LEADER = "Arrows: ";
+    const string TORCH_LEADER = "Torches: ";
+    
+    public TextMeshProUGUI arrowsText;
+    public TextMeshProUGUI woodText;
+    public TextMeshProUGUI torchesText;
+    public Slider healthBar;
+
+    public Player player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        arrowsText.text = $"{WOOD_LEADER}{player.wood}";
+        woodText.text = $"{ARROW_LEADER}{player.arrows}";
+        torchesText.text = $"{TORCH_LEADER}{player.torches}";
+        healthBar.value = (float)player.health / player.max_health;
+    }
+
+
+}
